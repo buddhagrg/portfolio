b7b6a963544e4cc9b37301a92df4b89b5a8d1c1b
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-03-18 17:14:29 +0545

    add social links

---
+++ src/app/data.js
@@ -0,0 +1,2 @@
+export const GITHUB_LINK = 'https://github.com/buddhagrg';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/buddhagrg/';
--- src/app/home/Hero.jsx
+++ src/app/home/Hero.jsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import { Col, Row } from "react-bootstrap";
 import ProfileImg from "../../assets/profile-img.jpg";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { GITHUB_LINK, LINKEDIN_LINK } from "../data";
 
 export default function Hero() {
     return (
         <Row className="hero-container">
-            <Col lg={{ span: 8, order: 0 }} xs={{ span: 12, order: 1 }} className="text-center text-md-start">
+            <Col lg={{ span: 8, order: 0 }} xs={{ span: 12, order: 1 }} className="text-left text-md-start">
                 <h4 className="my-4">Hi, I'm Buddha</h4>
-                <p>I'm a software engineer specializing in ReactJs.</p>
+                <p>
+                    I'm a software engineer specializing in ReactJs. I have passion for building interactive interfaces
+                    that are beautiful and functional for wide range of users.
+                </p>
+                <div className="d-flex">
+                    <a className="social-link" href={LINKEDIN_LINK}>
+                        <BsLinkedin />
+                    </a>
+                    <a className="social-link" href={GITHUB_LINK}>
+                        <BsGithub />
+                    </a>
+                </div>
             </Col>
             <Col lg={{ span: 4, order: 1 }} xs={{ span: 12, order: 0 }} className="text-center text-md-right">
                 <Image
