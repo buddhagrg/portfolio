b6ec8c640bd51e6982fb8a27ffef43027e2eeb5c
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-08-27 14:10:53 +0545

    update resume google docs link

--- src/app/data.js
+++ src/app/data.js
@@ -1,4 +1,4 @@
 export const GITHUB_LINK = 'https://github.com/buddhagrg';
 export const LINKEDIN_LINK = 'https://www.linkedin.com/in/buddhagrg/';
-export const RESUME_DOCS_LINK = 'https://docs.google.com/document/d/1dNEQJlDLNWvdxPCNb_I_6wZfNhCv3wq_MBB6qnuzlhQ/edit';
+export const RESUME_DOCS_LINK = 'https://docs.google.com/document/d/1-ezkWbZA9AE5gu8Ii1aKRGMBKhGscza2scOjCvrYoAQ/edit?usp=sharing';
 export const WORK_EMAIL_LINK = 'gurungbuddha6@outlook.com';
