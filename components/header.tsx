8f5d6580cba9eea4dd715f21840dd36dd4027fc3
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2025-06-10 15:02:51 +0545

    enhance ui/ux for header navs

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ToggleTheme } from "./toggle-theme";
+import clsx from "clsx";
 
 export function Header() {
   const pathname = usePathname();
@@ -14,31 +15,40 @@ export function Header() {
     { id: "#contact", name: "Contact" },
   ];
 
+  const Brand = (
+    <h4 className="text-xl font-medium text-foreground/70 hover:text-foreground shrink-0">
+      <Link href={"/"}>Buddha Gurung</Link>
+    </h4>
+  );
+
   return (
     <header className="md:py-4 fixed start-0 top-0 left-0 w-full bg-background-secondary z-50">
-      <div className="max-w-4xl mx-auto">
-        <div className="flex flex-col md:flex-row">
-          <h4 className="text-xl font-medium text-foreground/70 hover:text-foreground">
-            <Link href={"/"}>Buddha Gurung</Link>
-          </h4>
+      <div
+        className={clsx(
+          `max-w-4xl mx-auto flex flex-col md:flex-row justify-between md:items-center`,
+          isHomePage ? "flex-col" : "flex-row"
+        )}
+      >
+        {Brand}
 
-          <div
-            className={`flex items-center space-x-6 md:ml-auto mr-5 ${
-              isHomePage ? "" : "hidden"
-            }`}
-          >
-            {menus.map(({ id, name }) => (
-              <Link
-                href={id}
-                className="text-foreground/70 hover:text-foreground"
-                key={id}
-              >
-                {name}
-              </Link>
-            ))}
+        {isHomePage ? (
+          <nav className="flex items-center">
+            <div className="space-x-6 md:ml-auto mr-5">
+              {menus.map(({ id, name }) => (
+                <Link
+                  href={id}
+                  className="text-foreground/70 hover:text-foreground"
+                  key={id}
+                >
+                  {name}
+                </Link>
+              ))}
+            </div>
             <ToggleTheme />
-          </div>
-        </div>
+          </nav>
+        ) : (
+          <ToggleTheme />
+        )}
       </div>
     </header>
   );
